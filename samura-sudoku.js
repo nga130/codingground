ca95f37ab358b701ae2c3577fee7e264705f372b
nga130 <nga130.oct27@gmail.com>
2017-02-05 17:38:40 +0100

    Add files via upload
    Browser macros

---
+++ samura-sudoku.js
@@ -0,0 +1,18 @@
+﻿var load, i;
+var dateID = "";
+
+var load;
+load =  "CODE:";
+load +=  "SET !DATASOURCE E:/sudoku/samurai-sudoku/filename_list.csv" + "\n";
+load +=  "SET !DATASOURCE_LINE {{i}}" + "\n";
+load +=  "SET !EXTRACT {{!COL1}}" + "\n";
+
+for ( i = 1; i <= 122; i++) {
+    iimSet("i", i);
+    iimPlay(load);
+    dateID = iimGetLastExtract(0);
+
+    iimSet("DATEID", dateID);
+    iimPlay("run1.iim");
+    iimPlay("run2.iim");
+}
